3269ce5eeaef78b2f11fa2a2464c4cb5db69a84e
MadmanGFX <42655246+MadmanGFX@users.noreply.github.com>
2018-11-23 18:33:19 -0700

    Delete submit.php

--- complete/submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$a = $_POST['udid'] . "\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['product'];
-	mail("madmangfx@yahoo.com", "UDID", $a);
-	header('Location: finished.html');
-?>
-	
